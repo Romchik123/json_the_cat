096e5ea65c165beb441bbd3fb725db29a1a41316
rom <mozzieisme@gmail.com>
2021-11-04 20:13:27 -0400

    Add new files and modify files

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,23 @@
 const request = require("request");
 
-const argv = process.argv;
+// const argv = process.argv;
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${argv[2]}`,
-  (error, response, body) => {
-    // console.log("error:", error); // Print the error if one occurred
-    // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
-    // console.log("body:", body); // Print the HTML for the Google homepage.
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Requested breed is not found: ${error}`);
-    } else if (data[0].description) {
-      console.log(data[0].description);
+const fetchBreedDescription = function(breedName, callback) {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      // console.log("error:", error); // Print the error if one occurred
+      // console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+      // console.log("body:", body); // Print the HTML for the Google homepage.
+      const data = JSON.parse(body);
+      // callback(error, data);
+      if (data.length === 0) {
+        callback(`Requested breed is not found: ${error}`, null);
+      } else if (data[0].description) {
+        callback(undefined, data[0].description);
+      }
     }
-  }
-);
+  );
+};
+
+module.exports = { fetchBreedDescription };
